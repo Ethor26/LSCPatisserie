16bce6880ff6e8ad38f1bfdfeb025902ac3305c4
luisi <luis.alba.sanchez@efrei.net>
2021-10-04 17:50:43 +0200

    j'essais de me faire fonctionner

---
+++ cmake-build-debug/CMakeFiles/LSCPatisserie.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LSCPatisserie.dir/fonctions.c.obj"
+  "CMakeFiles/LSCPatisserie.dir/main.c.obj"
+  "LSCPatisserie.exe"
+  "LSCPatisserie.exe.manifest"
+  "LSCPatisserie.pdb"
+  "libLSCPatisserie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LSCPatisserie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
