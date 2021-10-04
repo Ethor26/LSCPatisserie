0a34ae1e1f503af451153b0eb663e566006750ea
maxim <maxime.goujon@efrei.net>
2021-10-04 19:02:31 +0200

    Fonctions sur les gateaux : test commit

--- Fonctions gateaux.c
+++ Fonctions gateaux.c
@@ -1,7 +1,7 @@
 #include "Fonctions gateaux.h"
+#include "stdlib.h"
 
-
-Element_str* initialiser_gouts(){
+Element_str* initialiser_gouts(){ // Commentaire inutile
 
 }
 
@@ -14,11 +14,32 @@ Element_str* traiter_commande(File_Commandes* f_commandes){
 }
 
 Gateau* creer_gateau(Element_str* commande){
-
+Gateau* G;
+G = (Gateau*) malloc(sizeof(Gateau));
+G->commande = commande;
+return G;
 }
 
 void construire_gateau(Gateau* gateau, Element_str* l_gouts){
-
+    Element_str* temp;
+    Element_str* temp2;
+    Element_str* temp3;
+    temp = l_gouts;
+    for(int i = 0; i< strlen(gateau->commande->texte); i++){
+        while(gateau->commande->texte[i] != temp->texte[0]){
+            temp = temp->next;
+        }
+        if(i==0){
+            temp2 = temp;
+            temp3 = temp2;
+        }
+        else{
+            temp2->next = temp;
+            temp2 = temp2->next;
+        }
+        temp = l_gouts;
+    }
+    gateau->p_gouts->Gouts = temp3;
 }
 
 void livrer(Gateau* gateau, File_Degustation* f_degustation){
--- cmake-build-debug/CMakeFiles/LSCPatisserie.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/LSCPatisserie.dir/DependInfo.cmake
@@ -1,25 +1,17 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "C"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "D:/ethan/OneDrive - Efrei/L2-S1/L2S1-INFORMATIQUE GENERALE/ALGORITHMIE et structure de donnees 2/TD/CLionProjectsL2S1/LSCPatisserie/Fonctions gateaux.c" "D:/ethan/OneDrive - Efrei/L2-S1/L2S1-INFORMATIQUE GENERALE/ALGORITHMIE et structure de donnees 2/TD/CLionProjectsL2S1/LSCPatisserie/cmake-build-debug/CMakeFiles/LSCPatisserie.dir/Fonctions_gateaux.c.obj"
-  "D:/ethan/OneDrive - Efrei/L2-S1/L2S1-INFORMATIQUE GENERALE/ALGORITHMIE et structure de donnees 2/TD/CLionProjectsL2S1/LSCPatisserie/Tools2.c" "D:/ethan/OneDrive - Efrei/L2-S1/L2S1-INFORMATIQUE GENERALE/ALGORITHMIE et structure de donnees 2/TD/CLionProjectsL2S1/LSCPatisserie/cmake-build-debug/CMakeFiles/LSCPatisserie.dir/Tools2.c.obj"
-  "D:/ethan/OneDrive - Efrei/L2-S1/L2S1-INFORMATIQUE GENERALE/ALGORITHMIE et structure de donnees 2/TD/CLionProjectsL2S1/LSCPatisserie/main.c" "D:/ethan/OneDrive - Efrei/L2-S1/L2S1-INFORMATIQUE GENERALE/ALGORITHMIE et structure de donnees 2/TD/CLionProjectsL2S1/LSCPatisserie/cmake-build-debug/CMakeFiles/LSCPatisserie.dir/main.c.obj"
+  "C:/Users/maxim/CLionProjects/LSCPatisserie/Fonctions gateaux.c" "C:/Users/maxim/CLionProjects/LSCPatisserie/cmake-build-debug/CMakeFiles/LSCPatisserie.dir/Fonctions_gateaux.c.obj"
+  "C:/Users/maxim/CLionProjects/LSCPatisserie/Tools2.c" "C:/Users/maxim/CLionProjects/LSCPatisserie/cmake-build-debug/CMakeFiles/LSCPatisserie.dir/Tools2.c.obj"
+  "C:/Users/maxim/CLionProjects/LSCPatisserie/main.c" "C:/Users/maxim/CLionProjects/LSCPatisserie/cmake-build-debug/CMakeFiles/LSCPatisserie.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  )
-
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
