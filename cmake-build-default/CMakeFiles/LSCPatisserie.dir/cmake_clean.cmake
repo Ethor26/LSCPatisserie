bfd9eed59c737d296ef83a6e82aa4ce7076679ef
Ethan SUISSA <ethan.suissa@efrei.net>
2021-10-04 16:37:32 +0200

    Initial commit

---
+++ cmake-build-default/CMakeFiles/LSCPatisserie.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LSCPatisserie.dir/main.c.obj"
+  "LSCPatisserie.exe"
+  "LSCPatisserie.exe.manifest"
+  "LSCPatisserie.pdb"
+  "libLSCPatisserie.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LSCPatisserie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
