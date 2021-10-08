b9a0e20d936d9aeac3aeb2b876a4cac45ea707b1
Ethan SUISSA <ethan.suissa@efrei.net>
2021-10-08 13:56:58 +0200

    Commit test

--- Fonctions gateaux.c
+++ Fonctions gateaux.c
@@ -208,6 +208,7 @@ int Interface_User(int *EcritCmd, Element_str * l_gouts, File_Commandes * f_comm
         case 2 : {
             if(*EcritCmd) {
                 printf("Reception de commande.\n"); // Affiche le choix proposé
+                construire_gateau(f_commandes->Gateaux->Gateau, f_commandes->Gateaux->Gateau->p_gouts->Gouts);
                 livrer(f_commandes->Gateaux->Gateau, f_degustations);
                 printf("Commande recue.");
             }
@@ -269,6 +270,7 @@ void Aleat_Choix(int *EcritCmd, Element_str * l_gouts, File_Commandes * f_comman
         case 2 : {
             printf("Reception de commande en cours.\n");// Affiche le choix proposé
             if(*EcritCmd) {
+                construire_gateau(f_commandes->Gateaux->Gateau, f_commandes->Gateaux->Gateau->p_gouts->Gouts);
                 livrer(f_commandes->Gateaux->Gateau, f_degustations);
                 printf("Commande recue.");
             }
